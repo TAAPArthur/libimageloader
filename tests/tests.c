8a9b851a91a8d9dda06924ddcb98c227eb3ef43e
Arthur Williams <taaparthur@disroot.org>
2021-11-25 20:51:40 -0800

    Add sorting test

--- tests/tests.c
+++ tests/tests.c
@@ -161,3 +161,27 @@ SCUTEST(open_invalid) {
     while(image_loader_get_num(default_context))
         image_loader_open(default_context, 0, NULL);
 }
+
+SCUTEST(sort_images) {
+    default_context = image_loader_create_context(0, 0, 0);
+    assert(image_loader_add_file(default_context, TEST_IMAGE_PATHS[0]));
+    int fd = open(TEST_IMAGE_PATHS[0], O_RDONLY | O_CLOEXEC);
+    // add from fd; the fd will be owned by the lib
+    assert(image_loader_add_from_fd(default_context, fd, "human_name"));
+    ImageLoaderData* image = NULL;
+    for(int i = 1; i < IMG_SORT_NUM; i++) {
+        image_loader_sort(default_context, i);
+        image = image_loader_open(default_context, 0, image);
+        const char* firstImage = image_loader_get_name(image);
+        image = image_loader_open(default_context, image_loader_get_num(default_context) -1, NULL);
+        const char* lastImage = image_loader_get_name(image);
+        // the negation sorts in the opposite direction
+        image_loader_sort(default_context, -i);
+        image = image_loader_open(default_context, image_loader_get_num(default_context) -1, NULL);
+        assert(strcmp(firstImage, image_loader_get_name(image)) == 0);
+        image = image_loader_open(default_context, 0, image);
+        assert(strcmp(lastImage, image_loader_get_name(image)) == 0);
+    }
+    // Shuffling; just verify this doesn't crash
+    image_loader_sort(default_context, 0);
+}
