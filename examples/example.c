7b3bd0de2b485ee05011d7d2f3181cd55e1bfd34
Arthur Williams <taaparthur@disroot.org>
2022-07-23 03:12:36 -0700

    example.c: use IMAGE_LOADER_REMOVE_INVALID flag
    Also update some logging

--- examples/example.c
+++ examples/example.c
@@ -7,12 +7,12 @@
  * Every row of the image will be printed space separated on the same line
  */
 int main(int argc, const char*argv[]) {
-    ImageLoaderContext* c = image_loader_create_context(argv + 1, 0, 0);
+    ImageLoaderContext* c = image_loader_create_context(argv + 1, 0, IMAGE_LOADER_REMOVE_INVALID);
     ImageLoaderData* current_image = NULL;
     for(int i = 0; i < image_loader_get_num(c); i++) {
         current_image = image_loader_open(c, i, current_image);
         if(current_image) {
-            printf("File: %s\n", image_loader_get_name(current_image));
+            printf("File: %s %d x %d \n", image_loader_get_name(current_image), image_loader_get_width(current_image), image_loader_get_height(current_image));
             const int* data = image_loader_get_data(current_image);
             for(int h = 0, n = 0; h < image_loader_get_height(current_image); h++) {
                 for(int w = 0; w < image_loader_get_width(current_image); w++, n++)
