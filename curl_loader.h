9e817555a6f9ed4bdb3e671882df03cca445dd8f
Arthur Williams <taaparthur@disroot.org>
2023-04-09 22:33:22 -0700

    Fix fd leak in curl loader

--- curl_loader.h
+++ curl_loader.h
@@ -14,12 +14,13 @@ static size_t write_data(void *ptr, size_t size, size_t nmemb, int*fd) {
 int curl_load(ImageLoaderContext* context, int _, ImageLoaderData* parent) {
     if (!strstr(parent->name, "://"))
         return -1;
+    int ret = -1;
     curl_global_init(CURL_GLOBAL_ALL);
 
     /* init the curl session */
     CURL* curl_handle = curl_easy_init();
     if (!curl_handle)
-        return -1;
+        goto end;
 
     /* set URL to get here */
     curl_easy_setopt(curl_handle, CURLOPT_URL, parent->name);
@@ -29,23 +30,26 @@ int curl_load(ImageLoaderContext* context, int _, ImageLoaderData* parent) {
     curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_data);
 
     int fd = image_loader_create_memory_file(parent->name, 0);
-    if (fd == -1) {
-        return fd;
-    }
+    if (fd == -1)
+        goto end;
     /* write the page body to this file handle */
     curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, &fd);
     /* get it! */
     if (CURLE_OK != curl_easy_perform(curl_handle)) {
-        return -1;
+        close(fd);
+        goto end;
     }
-
-    ImageLoaderData* data = image_loader_add_from_fd_with_flags(context, fd, parent->name, IMG_DATA_KEEP_OPEN);
     lseek(fd, 0, SEEK_SET);
 
+    ImageLoaderData* data = image_loader_add_from_fd_with_flags(context, fd, parent->name, IMG_DATA_KEEP_OPEN);
+    ret = 0;
+
+end:
     /* cleanup curl stuff */
     curl_easy_cleanup(curl_handle);
 
     curl_global_cleanup();
-    return 0;
+
+    return ret;
 }
 #endif
